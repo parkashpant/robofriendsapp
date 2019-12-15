f83011f88192614bad7defe326d00a186e5a5582
Parkash Pant <parkashpant1989@gmail.com>
2019-12-15 11:04:59 -0600

    Updates

---
+++ precache-manifest.e8a446bcb1df1677d0f6dfb438ae0251.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f5261ba729d4aa46177",
+    "url": "/robofriendsapp/static/css/main.979f760b.chunk.css"
+  },
+  {
+    "revision": "5f5261ba729d4aa46177",
+    "url": "/robofriendsapp/static/js/main.b0acbfbc.chunk.js"
+  },
+  {
+    "revision": "8f2c2ea2dbc031d097a7",
+    "url": "/robofriendsapp/static/js/runtime~main.d4dbc020.js"
+  },
+  {
+    "revision": "e4eb319e2b87e0e906e5",
+    "url": "/robofriendsapp/static/css/2.ce6f0ec1.chunk.css"
+  },
+  {
+    "revision": "e4eb319e2b87e0e906e5",
+    "url": "/robofriendsapp/static/js/2.48337ee6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsapp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8b368fe396e72dbff7aa8ab2ec252565",
+    "url": "/robofriendsapp/index.html"
+  }
+];
